fc9ee9d5062a8a0ab52baf883b69096b6c420707
Yain aitHamid <yassinaithamid@gmail.com>
2022-05-31 14:12:50 +0100

    with profile page infos

---
+++ server/middle_example/src/application.ts
@@ -0,0 +1,68 @@
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {ServiceMixin} from '@loopback/service-proxy';
+import path from 'path';
+import {MySequence} from './sequence';
+
+import {AuthenticationComponent, registerAuthenticationStrategy} from '@loopback/authentication';
+import {
+  JWTAuthenticationComponent,
+  UserServiceBindings,
+} from '@loopback/authentication-jwt';
+import {MongoDataSource} from './datasources';
+import { JWTService } from './authorization/services/JWT.services';
+import { MyAuthBindings, PasswordHasherBindings } from './authorization/keys';
+import { BcryptHasher } from './authorization/services/hash.password.bcryptjs';
+
+import { UserRoleProvider } from './providers/user-role.provider';
+import { JWTStrategy } from './authorization/strategy/JWT.strategy';
+export {ApplicationConfig};
+
+export class Middle extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    super(options);
+
+    this.component(AuthenticationComponent);
+    // Mount jwt component
+    this.component(JWTAuthenticationComponent);
+    // Bind datasource
+    this.dataSource(MongoDataSource, UserServiceBindings.DATASOURCE_NAME);
+
+    // Set up the custom sequence
+    this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+    this.component(AuthenticationComponent);
+    // Bind JWT & permission authentication strategy related elements
+    registerAuthenticationStrategy(this, JWTStrategy);
+    this.bind(MyAuthBindings.TOKEN_SERVICE).toClass(JWTService);
+    this.bind(MyAuthBindings.USER_ROLE).toProvider(UserRoleProvider);
+    this.bind(PasswordHasherBindings.ROUNDS).to(10);
+    this.bind(PasswordHasherBindings.PASSWORD_HASHER).toClass(BcryptHasher);
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+    };
+  }
+}
---
+++ server/middle_example/src/controllers/user.controller.ts
@@ -0,0 +1,241 @@
+import {authenticate, AuthenticationBindings} from '@loopback/authentication';
+import {Getter, inject} from '@loopback/core';
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where
+} from '@loopback/repository';
+import {
+  del, get,
+  getModelSchemaRef, HttpErrors, param, patch, post, put, requestBody,
+  response
+} from '@loopback/rest';
+import {
+  Credential, JWTService, MyAuthBindings, MyUserProfile, PasswordHasher, PasswordHasherBindings, Role
+} from '../authorization';
+/*import {MyAuthBindings, PasswordHasherBindings} from '../authorization/keys';
+import {Role} from '../authorization/role';
+import {PasswordHasher} from '../authorization/services/hash.password.bcryptjs';
+import {JWTService} from '../authorization/services/JWT.services';
+import {
+  MyUserProfile,
+  Credential,
+  CredentialsRequestBody,
+} from '../authorization/types';*/
+import {User} from '../models/user.model';
+import {UserRepository} from '../repositories/user.repository';
+
+//import { CredentialsRequestBody } from './user.controller';
+
+export class UserController {
+  constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+    @inject(PasswordHasherBindings.PASSWORD_HASHER)
+    public passwordHasher: PasswordHasher,
+    @inject(MyAuthBindings.TOKEN_SERVICE)
+    public jwtService: JWTService,
+    @inject.getter(AuthenticationBindings.CURRENT_USER)
+    public getCurrentUser: Getter<MyUserProfile>,
+  ) {}
+
+  //@authenticate({strategy: 'jwt', options: {option1: Role.Admin}})
+  @post('/users', {
+    responses: {
+      '200': {
+        description: 'User model instance',
+        content: {'application/json': {schema: getModelSchemaRef(User)}},
+      },
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {
+            title: 'NewUser',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    user: Omit<User, 'id'>,
+  ): Promise<User> {
+    if (await this.userRepository.findOne({where: {email: user.email}})) {
+      throw new HttpErrors.BadRequest(`This email already exists`);
+    } else {
+      user.password = await this.passwordHasher.hashPassword(
+        user.password || '',
+      );
+      const newUser = await this.userRepository.create(user);
+      delete newUser.password;
+      return newUser;
+    }
+  }
+
+  @post('/users/login', {
+    responses: {
+      '200': {
+        description: 'Token',
+        content: {
+          'application/json': {},
+        },
+      },
+    },
+  })
+  async login(
+    @requestBody({
+      description: 'The input of login function',
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            required: ['email', 'password'],
+            properties: {
+              email: {
+                type: 'string',
+                format: 'email',
+              },
+              password: {
+                type: 'string',
+                minLength: 8,
+              },
+            },
+          },
+        },
+      },
+    })
+    credential: Credential,
+  ): Promise<{
+    token: string;
+    username: string;
+    email: string;
+    role: Role;
+    id: string;
+  }> {
+    const user = await this.jwtService.verifyCredential(credential);
+    const token = await this.jwtService.generateToken({
+      role: user.role,
+      id: user.id,
+    } as MyUserProfile);
+    return {
+      token,
+      username: user.username!!,
+      email: user.email!!,
+      role: user.role!!,
+      id: user.id,
+    };
+  }
+
+
+ // @authenticate({strategy: 'jwt', options: {option1: Role.Admin}})
+  @get('/users/count')
+  @response(200, {
+    description: 'User model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(@param.where(User) where?: Where<User>): Promise<Count> {
+    return this.userRepository.count(where);
+  }
+
+  //@authenticate({strategy: 'jwt', options: {option1: Role.Admin}})
+  @get('/users')
+  @response(200, {
+    description: 'Array of User model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(User, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(@param.filter(User) filter?: Filter<User>): Promise<User[]> {
+    return this.userRepository.find(filter);
+  }
+
+  //@authenticate({strategy: 'jwt', options: {option1: Role.Admin}})
+  @patch('/users')
+  @response(200, {
+    description: 'User PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {partial: true}),
+        },
+      },
+    })
+    user: User,
+    @param.where(User) where?: Where<User>,
+  ): Promise<Count> {
+    return this.userRepository.updateAll(user, where);
+  }
+
+
+  //whoami
+
+  @get('/users/{id}')
+  @response(200, {
+    description: 'User model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(User, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(User, {exclude: 'where'}) filter?: FilterExcludingWhere<User>,
+  ): Promise<User> {
+    return this.userRepository.findById(id, filter);
+  }
+
+  @patch('/users/{id}')
+  @response(204, {
+    description: 'User PATCH success',
+  })
+  async updateById(
+    @param.path.number('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {partial: true}),
+        },
+      },
+    })
+    user: User,
+  ): Promise<void> {
+    await this.userRepository.updateById(id, user);
+  }
+
+
+
+
+  @put('/users/{id}')
+  @response(204, {
+    description: 'User PUT success',
+  })
+  async replaceById(
+    @param.path.string('id') id: string,
+    @requestBody() user: User,
+  ): Promise<void> {
+    await this.userRepository.replaceById(id, user);
+  }
+
+  @authenticate({strategy: 'jwt', options: {option1: Role.Admin}})
+  @del('/users/{id}')
+  @response(204, {
+    description: 'User DELETE success',
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.userRepository.deleteById(id);
+  }
+}
